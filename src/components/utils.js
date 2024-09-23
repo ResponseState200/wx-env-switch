9c306e73275cf2243156d4e538a90e519d1c637a
zhengyan <529968905@qq.com>
2024-09-23 14:28:12 +0800

    feature: 增加兼容H5与App,但是切换环境的组件需要自己编写

--- src/components/utils.js
+++ src/components/utils.js
@@ -13,13 +13,36 @@
  */
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.getEnvList = exports.getCurrentEnvData = exports.exit = exports.changeEnv = exports.init = void 0;
-var sessionKeyStr = 'switchEnvDevelopKey';
 var currentEnvData = undefined;
-var init = function (envArr) {
-    wx.setStorageSync('switchEnvListOnly', envArr);
-    var wxEnvVersion = wx.getAccountInfoSync().miniProgram.envVersion; // 微信当前环境
-    var currentEnvKey = wx.getStorageSync(sessionKeyStr);
-    currentEnvData = currentEnvKey ? envArr.find(function (t) { return t.value === currentEnvKey; }) : envArr.find(function (t) { return t.wxEnvVersion === wxEnvVersion; });
+var envDataStoreKey = 'currentEnvData';
+// 根据环境选择使用 uni 或 wx 的存储方法
+var setStorageSync = function (key, value) {
+    if (typeof uni !== 'undefined') {
+        uni.setStorageSync(key, value);
+    }
+    else if (typeof wx !== 'undefined') {
+        wx.setStorageSync(key, value);
+    }
+};
+var getStorageSync = function (key) {
+    if (typeof uni !== 'undefined') {
+        return uni.getStorageSync(key);
+    }
+    else if (typeof wx !== 'undefined') {
+        return wx.getStorageSync(key);
+    }
+    return null; // 防止返回 undefined
+};
+var init = function (envArr, defaultWxEnvVersion) {
+    if (defaultWxEnvVersion === void 0) { defaultWxEnvVersion = 'develop'; }
+    setStorageSync('switchEnvListOnly', envArr);
+    // 默认使用 develop 环境，微信小程序通过 wx.getAccountInfoSync 获取环境
+    var wxEnvVersion = defaultWxEnvVersion;
+    if (typeof wx !== 'undefined' && typeof wx.getAccountInfoSync === 'function') {
+        wxEnvVersion = wx.getAccountInfoSync().miniProgram.envVersion;
+    }
+    currentEnvData = getStorageSync(envDataStoreKey) || envArr.find(function (t) { return t.wxEnvVersion === wxEnvVersion; });
+    setStorageSync(envDataStoreKey, currentEnvData);
     return currentEnvData;
 };
 exports.init = init;
@@ -29,19 +52,24 @@ var changeEnv = function (value) {
         throw new Error("传入的值在配置数组中不存在,请检查!");
         return;
     }
-    wx.setStorageSync(sessionKeyStr, value);
+    setStorageSync(envDataStoreKey, curData);
     currentEnvData = curData;
 };
 exports.changeEnv = changeEnv;
 var exit = function () {
-    wx.exitMiniProgram();
+    if (typeof wx !== 'undefined' && typeof wx.exitMiniProgram === 'function') {
+        wx.exitMiniProgram();
+    }
+    else if (typeof uni !== 'undefined') {
+        uni.reLaunch({ url: '/pages/index/index' }); // 示例，重新启动应用到首页
+    }
 };
 exports.exit = exit;
 var getCurrentEnvData = function () {
-    return currentEnvData;
+    return getStorageSync(envDataStoreKey);
 };
 exports.getCurrentEnvData = getCurrentEnvData;
 var getEnvList = function () {
-    return wx.getStorageSync('switchEnvListOnly') || [];
+    return getStorageSync('switchEnvListOnly') || [];
 };
 exports.getEnvList = getEnvList;
--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -11,42 +11,69 @@
  * @param sessionKey string 存放在sessionStorage中的key值
  */
 
+declare const uni: any;
 interface EnvObj {
-  label:string,
-  value:string,
-  wxEnvVersion: 'develop' | 'trial' | 'release',
+  label: string;
+  value: string;
+  wxEnvVersion: 'develop' | 'trial' | 'release';
   [key: string]: any; // 表示可以有其他任意的可选属性
 }
 
-let sessionKeyStr:string = 'switchEnvDevelopKey'
-let currentEnvData:EnvObj | undefined = undefined;
-export const init = (envArr:EnvObj[]): EnvObj | undefined => {
-  wx.setStorageSync('switchEnvListOnly',envArr)
-  const wxEnvVersion = wx.getAccountInfoSync().miniProgram.envVersion // 微信当前环境
-  const currentEnvKey = wx.getStorageSync(sessionKeyStr)
-  currentEnvData = currentEnvKey ? envArr.find((t:EnvObj) => t.value === currentEnvKey) : envArr.find((t:EnvObj) => t.wxEnvVersion === wxEnvVersion)
-  return currentEnvData
+let currentEnvData: EnvObj | undefined = undefined;
+const envDataStoreKey  = 'currentEnvData'
+
+// 根据环境选择使用 uni 或 wx 的存储方法
+const setStorageSync = (key: string, value: any) => {
+  if (typeof uni !== 'undefined') {
+    uni.setStorageSync(key, value);
+  } else if (typeof wx !== 'undefined') {
+    wx.setStorageSync(key, value);
+  }
 };
 
-export const changeEnv = (value:string)=>{
-  const curData = getEnvList().find((t:EnvObj) => t.value === value)
-  if(!curData){
-    throw new Error("传入的值在配置数组中不存在,请检查!");
-    return
+const getStorageSync = (key: string) => {
+  if (typeof uni !== 'undefined') {
+    return uni.getStorageSync(key);
+  } else if (typeof wx !== 'undefined') {
+    return wx.getStorageSync(key);
   }
-  wx.setStorageSync(sessionKeyStr,value)
+  return null; // 防止返回 undefined
+};
+
+export const init = (envArr: EnvObj[],defaultWxEnvVersion='develop'): EnvObj | undefined => {
+  setStorageSync('switchEnvListOnly', envArr);
+  // 默认使用 develop 环境，微信小程序通过 wx.getAccountInfoSync 获取环境
+  let wxEnvVersion: string = defaultWxEnvVersion;
+  if (typeof wx !== 'undefined' && typeof wx.getAccountInfoSync === 'function') {
+    wxEnvVersion = wx.getAccountInfoSync().miniProgram.envVersion;
+  }
+  currentEnvData = getStorageSync(envDataStoreKey) || envArr.find((t: EnvObj) => t.wxEnvVersion === wxEnvVersion);
+  setStorageSync(envDataStoreKey, currentEnvData);
+  return currentEnvData;
+};
+
+export const changeEnv = (value: string) => {
+  const curData = getEnvList().find((t: EnvObj) => t.value === value);
+  if (!curData) {
+    throw new Error("传入的值在配置数组中不存在,请检查!");
+    return;
+  }
+  setStorageSync(envDataStoreKey, curData);
   currentEnvData = curData;
-}
+};
 
-export const exit = ()=>{
-  wx.exitMiniProgram()
-}
+export const exit = () => {
+  if (typeof wx !== 'undefined' && typeof wx.exitMiniProgram === 'function') {
+    wx.exitMiniProgram();
+  } else if (typeof uni !== 'undefined') {
+    uni.reLaunch({ url: '/pages/index/index' }); // 示例，重新启动应用到首页
+  }
+};
 
-export const getCurrentEnvData = ()=>{
-  return currentEnvData
-}
+export const getCurrentEnvData = () => {
+  return getStorageSync(envDataStoreKey);
+};
 
-export const getEnvList = ()=>{
-  return wx.getStorageSync('switchEnvListOnly') || []
-}
-
+export const getEnvList = () => {
+  return getStorageSync('switchEnvListOnly') || [];
+};
